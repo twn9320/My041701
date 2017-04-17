40b9d21a9b20f2752fc4b532e9be7c450cf36b4f
twn9320 <twn9320@gmail.com>
2017-04-17 15:13:47 +0800

    Gson +ArrayList

---
+++ app/src/main/java/com/example/yvtc/my041701/Animal.java
@@ -0,0 +1,14 @@
+package com.example.yvtc.my041701;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by YVTC on 2017/4/17.
+ */
+
+public class Animal {
+    public String district;
+    public String address;
+    public String tel;
+    public String opening_hours;
+}
--- app/src/main/java/com/example/yvtc/my041701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my041701/MainActivity.java
@@ -3,23 +3,31 @@ package com.example.yvtc.my041701;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ListView lv ;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView)findViewById(R.id.listView);
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
@@ -27,17 +35,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         Log.d("NET", response);
 
-                        try {
-                            JSONArray array = new JSONArray(response);
-                            int i;
-                            for(i=0;i<array.length();i++){
-                                JSONObject obj = array.getJSONObject(i);
-                                String s= obj.getString("district");
-                                Log.d("NET",s);
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        Gson gson = new Gson();
+                        ArrayList<Animal> mylist = gson.fromJson(response,new TypeToken<ArrayList<Animal>>(){}.getType());
+                        ArrayList<String> strlist = new ArrayList<>();
+                        for(Animal a:mylist){
+                            Log.d("NET",a.district);
+                            strlist.add(a.district);
                         }
+                        adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,strlist);
+                        lv.setAdapter(adapter);
+
                     }
                 }, new Response.ErrorListener() {
             @Override
